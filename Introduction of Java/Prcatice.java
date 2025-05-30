b22da1e6bdaa1a143402da7822c1b2d945a73942
gaurav13407 <gaurav9997961651jjk277584@outlook.com>
2025-05-30 10:43:24 +0530

    Introductiopn Of Java

---
+++ Introduction of Java/Prcatice.java
@@ -0,0 +1,18 @@
+public class Prcatice{
+    public static void main(String[] args) {
+        System.out.println("Hello My name is Gaurav And My Age is 19");
+        System.out.println("Hiu There I am Learing Java");
+
+        //Printing the reslt of 5+3
+        System.out.print("The result of 5+3 is:");
+        System.out.println(5 + 3);
+
+        //Using  \n to go to the other line
+        System.out.println("Hi There I am Learing \n Java");
+        
+        //Creating a Formate table using \t 
+        System.out.println("\nName\tAge\tCountry");
+        System.out.println("Gaurav\t19\tIndia");
+        System.out.println("John\t20\tUSA");
+    }
+}
