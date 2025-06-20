e08b06ffec044dda373d5492528dd5d2b770b084
gaurav13407 <9997961651jjk277584@outlook.com>
2025-06-20 09:23:19 +0530

    UPdate

---
+++ Condidtional Statement/Weather.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Weather {
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("Welcome to the Galatic Weather Advisor");
+        System.out.println("Please Enter the planent you are on(Earth,Mars,Vens,Jupiter):");
+
+        String planet=scanner.nextLine().trim().toLowerCase();
+
+        System.out.println("Please Enter the the current Tempearture in celsiuis:");
+        double temprature=scanner.nextDouble();
+
+        String advice="";
+
+        if(planet.equals("earth")){
+            if(temprature<0){
+                advice="its freezing on Earth!wear a heavy coat";
+            }
+            else if(temprature<15){
+                advice="Its quite chiily on Earth.Wear A jacket";
+            }
+            else if(temprature<25){
+                advice="";
+            }
+        }
+        System.out.println(advice);
+        scanner.close();
+    }
+}
